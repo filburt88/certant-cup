07d4495df3d81cb7b3f297b1d27a2b9608ad7ac1
Franco Osores <franco_osores@hotmail.com>
2022-09-26 11:28:46 -0300

    Set proxy for development

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
 })
-
 export class LoginFormComponent implements OnInit {
   form!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private authServ: AuthService) {}
 
   ngOnInit(): void {
     this.form = this.fb.group({
-      usuario: '',
+      username: '',
       contrasenia: '',
+    });
+  }
+
+  test() {
+    console.log(this.form.value);
+  }
+
+  login() {
+    this.authServ.login(this.form.value).subscribe({
+      next: (res) => {
+        console.log(res);
+      },
     });
   }
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  // headers = new HttpHeaders({
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+  //   'Access-Control-Allow-Headers':
+  //     'X-Requested-With, content-type, Authorization',
+  //   Accept: 'application/json',
+  //   observe: 'response',
+  // });
+
+  login(user: any): Observable<any> {
+    return this.http.post('/fixture/login-usuario', user, {
+      withCredentials: true,
+    });
+  }
 }

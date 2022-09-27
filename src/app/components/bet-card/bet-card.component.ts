8509cad063900470736639993dfe07cc2119d55a
Franco Osores <franco_osores@hotmail.com>
2022-09-27 13:23:07 -0300

    Layout for bet card finished

--- src/app/components/bet-card/bet-card.component.ts
+++ src/app/components/bet-card/bet-card.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bet-card.component.scss']
 })
 export class BetCardComponent implements OnInit {
+  test = true
 
   constructor() { }
 
